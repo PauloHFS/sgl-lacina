7f5ed08bfc7e421d266301de18f5e4f1cba559bd
Paulo Hernane <paulohernane10@gmail.com>
2025-04-16 04:14:57 +0000

    refactor: rename Colaborador interface to User; streamline user properties and remove unused fields

--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -1,34 +1,27 @@
-export interface Colaborador {
-    id: number;
-    linkedin?: string;
-    github?: string;
-    figma?: string;
-    foto?: string;
-    curriculo?: string;
-    area_atuacao?: string;
-    tecnologias?: string;
-    cpf?: string;
-    conta_bancaria?: string;
-    agencia?: string;
-    codigo_banco?: string;
-    rg?: string;
-    uf_rg?: string;
-    telefone?: string;
-    created_at?: string;
-    updated_at?: string;
-}
-
-export interface Docente {
-    id: number;
-}
-
 export interface User {
     id: number;
     name: string;
     email: string;
     email_verified_at?: string;
-    colaborador?: Colaborador;
-    docente?: Docente;
+
+    linkedin_url?: string;
+    github_url?: string;
+    figma_url?: string;
+    foto_url?: string;
+    curriculo?: string;
+    area_atuacao?: string;
+    tecnologias?: string;
+
+    cpf?: string;
+    rg?: string;
+    uf_rg?: string;
+    orgao_emissor_rg?: string;
+    conta_bancaria?: string;
+    agencia?: string;
+    codigo_banco?: string;
+
+    created_at?: string;
+    updated_at?: string;
 }
 
 export type PageProps<
