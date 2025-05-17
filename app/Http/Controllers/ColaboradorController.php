a0cddd1e2b5a3db31d22ca188e1d40f3353ed98e
Paulo Hernane <paulohernane10@gmail.com>
2025-05-17 22:33:04 +0000

    fix: remove commented debug code from ColaboradorController

--- app/Http/Controllers/ColaboradorController.php
+++ app/Http/Controllers/ColaboradorController.php
@@ -18,35 +18,6 @@ class ColaboradorController extends Controller
 {
     public function index(Request $request)
     {
-        // Log::debug('Busca de colaboradores iniciada', [
-        //     'search' => $request->input('search'),
-        // ]);
-
-        // $search = $request->input('search', '');
-
-        // if (!empty($search)) {
-        //     $colaboradores = Colaborador::search($search)
-        //         ->query(function ($builder) {
-        //             $builder->join('users', 'colaboradores.id', '=', 'users.id')
-        //                 ->select('colaboradores.*', 'users.name', 'users.email');
-        //         })
-        //         ->paginate(10)
-        //         ->appends($request->input('search'));
-        // } else {
-        //     $colaboradoresQuery = Colaborador::query()
-        //         ->join('users', 'colaboradores.id', '=', 'users.id')
-        //         ->select('colaboradores.*', 'users.name', 'users.email');
-
-        //     $colaboradores = $colaboradoresQuery
-        //         ->orderBy('colaboradores.created_at', 'desc')
-        //         ->paginate(10)
-        //         ->appends($request->input('search'));
-        // }
-
-        // Log::debug('Busca de colaboradores concluída', [
-        //     'total' => $colaboradores->total(),
-        // ]);
-
         $status = $request->input('status');
 
         $usuarios = null;
