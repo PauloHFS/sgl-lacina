3dc1d5ee5be5be68fa506672ed04ed4fd48c55c1
Paulo Hernane <paulohernane10@gmail.com>
2025-05-28 00:48:46 +0000

    refactor: update VinculoAceito email content definition to use markdown view

--- app/Mail/VinculoAceito.php
+++ app/Mail/VinculoAceito.php
@@ -42,7 +42,7 @@ class VinculoAceito extends Mailable
     public function content(): Content
     {
         return new Content(
-            view: 'emails.vinculo-aceito',
+            markdown: 'emails.vinculo-aceito',
             with: [
                 'colaborador' => $this->colaborador,
                 'projeto' => $this->projeto,
