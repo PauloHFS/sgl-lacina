0c87943ee9c2af8735883a67430bf7ec88670330
Paulo Hernane <paulohernane10@gmail.com>
2025-05-30 22:41:28 +0000

    refactor: update authentication tests to use consistent password and correct redirect route

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -8,6 +8,7 @@ test('registration screen can be rendered', function () {
 
 test('new users can register', function () {
     $response = $this->post('/register', [
+        'lab_password' => '1234',
         'name' => 'Test User',
         'email' => 'test@example.com',
         'password' => 'password',
@@ -15,5 +16,5 @@ test('new users can register', function () {
     ]);
 
     $this->assertAuthenticated();
-    $response->assertRedirect(route('dashboard', absolute: false));
+    $response->assertRedirect(route('pos-cadastro', absolute: false));
 });
