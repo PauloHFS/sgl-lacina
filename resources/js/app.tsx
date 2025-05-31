9a12a6c1de21eccb33f4607fd0d233bd47b6545c
Paulo Hernane <paulohernane10@gmail.com>
2025-05-31 04:21:26 +0000

    refactor: remove beta notification and tooltip from app layout

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -21,6 +21,32 @@ createInertiaApp({
         root.render(
             <ToastProvider>
                 <App {...props} />
+                <div className="toast toast-center toast-bottom z-50">
+                    <div role="alert" className="alert alert-warning">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="h-6 w-6 shrink-0 stroke-current"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
+                            />
+                        </svg>
+                        <span>Esta é uma versão beta.</span>
+                        <div
+                            className="tooltip tooltip-top tooltip-primary"
+                            data-tip="Esta é uma versão beta. Algumas funcionalidades podem estar incompletas ou apresentar instabilidade. Agradecemos o seu feedback!"
+                        >
+                            <button className="btn btn-circle btn-outline btn-sm">
+                                ?
+                            </button>
+                        </div>
+                    </div>
+                </div>
             </ToastProvider>,
         );
     },
