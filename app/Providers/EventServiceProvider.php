2a973462afa43c1cad88ba645ec950cca42ed7e3
Paulo Hernane <paulohernane10@gmail.com>
2025-07-05 00:46:02 +0000

    feat: add EventServiceProvider to handle job and task event logging to Discord

---
+++ app/Providers/EventServiceProvider.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Providers;
+
+use App\Listeners\LogJobFailedToDiscord;
+use App\Listeners\LogJobProcessedToDiscord;
+use App\Listeners\LogScheduledTaskFinishedToDiscord;
+use Illuminate\Console\Events\ScheduledTaskFinished;
+use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+use Illuminate\Queue\Events\JobFailed;
+use Illuminate\Queue\Events\JobProcessed;
+
+class EventServiceProvider extends ServiceProvider
+{
+    /**
+     * The event listener mappings for the application.
+     */
+    protected $listen = [
+        JobProcessed::class => [
+            LogJobProcessedToDiscord::class,
+        ],
+        JobFailed::class => [
+            LogJobFailedToDiscord::class,
+        ],
+        ScheduledTaskFinished::class => [
+            LogScheduledTaskFinishedToDiscord::class,
+        ],
+    ];
+
+    /**
+     * Register any events for your application.
+     */
+    public function boot(): void
+    {
+        parent::boot();
+    }
+
+    /**
+     * Determine if events and listeners should be automatically discovered.
+     */
+    public function shouldDiscoverEvents(): bool
+    {
+        return false;
+    }
+}
